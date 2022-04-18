8fdad318f65684e8faa19c903426a463b73f0543
Redstone-RM <solutionforge@gmail.com>
2022-04-18 09:17:34 -0600

    Dev (#7)
    * Working I2C Slave with ROS2 Twist msg
    
    * working twist i2c poc
    
    * ctrlmsg working
    
    * working with serial transfer i2c lib
    
    * working on i2c response
    
    * Cleanup 1 motor ctrl libdeps for most includes
    
    * cleanup main and move sensor code to sensors.h
    
    * Update README
    
    * Update README.md
    
    * removed from tracking
    
    * Add helper.h move config and  some functions here
    
    Co-authored-by: username

---
+++ include/helper.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <Arduino.h>
+#include <stdio.h> // LMX 
+#include <task.h> // LMX
+#include <log.h> // LMX
+#include <sysclock.h> // LMX
+
+#define VERSION "Bum Biter MK-2.2.0"
+
+/*######################## <libtask LMX dpa> #########################################*/
+#define BAUDRATE 57600  // LMX 
+#define PRINTF Serial.println // LMX 
+#define SPRINTF sprintf // LMX 
+
+/* ------------ <dpa> -------------------------- */
+/* Count idle cycles per second dpa original function */ 
+unsigned long idle_cnt;
+void cpu_idle(ASIZE ignored){
+    unsigned long t;
+    unsigned long cnt;
+    
+    t = sysclock + 1000;
+    while (1) {
+        idle_cnt = proc_counter;
+        proc_counter = 0;
+        // WAIT(1000);
+        PERIOD(&t,1000);
+    }
+}
+
+void stats_task(ASIZE delay) {
+    TSIZE t;
+    t = sysclock + delay;
+
+    while (1) {
+      // WAIT(delay);
+      PERIOD(&t,delay);
+      PRINTF("");
+      SPRINTF(sbuf,"# Sysclock\t%ld\tSampleclock\t%ld\tIdleHz\t%ld",
+              sysclock,sampleclock,idle_cnt);
+      PRINTF(sbuf);
+      PRINTF("");
+      print_llist(1);
+    }
+}
+
+
+
+/* 
+Choose one of the below:  
+#define WAIT(d)  { d *= 10; cnt = 0; while (cnt++ < d) defer(); }
+#define WAIT(d)  { msleep(d); }
+#define WAIT(d)  { wake_after(d); }
+*/
+#define WAIT(d)  { wake_after(d); } // LMX 
+
+
+
+/* ----------- Functions --------------------------- */
+
+bool randomBool() {
+   return rand() > (RAND_MAX / 2);
+}
+
+float clip(float value, float min, float max ){ // dpa inspired cliping function
+  if (value > max ) return max;
+  if (value < min ) return min;
+  return value;
+}
+
+/* ------------ <dpa> -------------------------- */
+/* Create signon and terminate task */
+void signon(ASIZE version)
+{
+  PRINTF(VERSION);
+  wake_after(2000);
+  PRINTF("# SIGNON Messages signing off!\n");
+  DELAY(1000);
+  terminate();
+}
+
